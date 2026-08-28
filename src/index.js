b66972a6855d87285c7d4d1647a0a9f51978bf4a
nonduduzongubane09-cpu <nonduduzongubane09@gmail.com>
2026-08-28 09:17:21 +0200

    Update index.js
    put on function

--- src/index.js
+++ src/index.js
@@ -2,7 +2,21 @@ function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    cityElement.textContent = searchInput.value;
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+    let apiKey = "f8476c7fdoae40f4ct738bbc6b5e9bc3";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(refreshWeather);
+}
